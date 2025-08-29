9111a3fd785476ca741a835272bbcc7e9567716a
yunseo0219 <chldbstj021902@gmail.com>
2025-08-29 20:54:15 +0900

    [FIX] cors 설정

---
+++ src/main/java/com/example/color_walk/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.color_walk.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "https://sinmungo.shop")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
